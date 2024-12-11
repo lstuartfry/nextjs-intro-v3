a2bec8ca3df85ca1cb94df6fb54ccbc5575fd053
Lukas Stuart-Fry <lstuartfry@gmail.com>
2024-12-11 14:56:51 -0800

    adds loading and error pages to todos route

---
+++ src/app/(dashboard)/todos/error.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function ErrorTodos() {
+  return <div>todos error</div>;
+}
--- src/app/(dashboard)/todos/page.tsx
+++ src/app/(dashboard)/todos/page.tsx
@@ -2,7 +2,10 @@ import db from '@/utils/db';
 import TodoList from '@/components/TodoList';
 
 async function getTodos() {
-  return await db.todo.findMany({});
+  // tests loading UI
+  await new Promise((resolve) => setTimeout(() => resolve(null), 2000));
+  const todos = await db.todo.findMany({});
+  return todos;
 }
 
 export default async function TodosPage() {
